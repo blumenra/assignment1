53af19066cbb6a399e55d871451b1e66583a64c7
blumenreich alon <blumenra@cs307row1-5.cs.bgu.ac.il>
2016-11-25 02:49:00 +0200

    Modefied for card class testing

--- src/reviiyot.cpp
+++ src/reviiyot.cpp
@@ -1,26 +1,60 @@
 #include <iostream>
 
-#include <Game.h>
+// #include <Game.h>
+
+
+#include "../include/Card.h"
+// #include <Card.h>
 
 using namespace std;
 
 int main(int argc, char **argv) {
-	char* configurationFile = argv[1];
 
-	Game game = Game(configurationFile);
-	game.init();
- 	Game initializedGame = game;
- 	game.play();  
+cout << "start main:" << endl;
 
-	cout << std::endl;
- 	game.printWinner();
-	game.printNumberOfTurns();
-	cout << "----------" << endl;
-	cout<<"Initial State:"<<endl;
-  	initializedGame.printState();
-	cout<<"----------"<<endl;
-	cout<<"Final State:"<<endl;
-	game.printState();
+cout << "creating new shape of Heart.." << endl;
+Shape shape(Heart);
+
+cout << "creating new figure of Ace.." << endl;
+Figure figure(Ace);
+
+cout << "creating new figureCard.." << endl;
+Card* figuredCard = new FigureCard(shape, figure);
+
+cout << "toString of card: " << endl;
+cout << figuredCard->toString() << endl;
+
+cout << "getFigure.." << endl;
+cout << figuredCard->getFigure() << endl;
+
+
+
+cout << "creating new numericCard.." << endl;
+Card* numericCard = new NumericCard(shape, 4);
+
+cout << "toString of card: " << endl;
+cout << numericCard->toString() << endl;
+
+cout << "getNumber.." << endl;
+cout << numericCard->getNumber() << endl;
+
+	// char* configurationFile = argv[1];
+
+	// Game game = Game(configurationFile);
+	// game.init();
+ // 	Game initializedGame = game;
+ // 	game.play();  
+
+	// cout << std::endl;
+ // 	game.printWinner();
+	// game.printNumberOfTurns();
+	// cout << "----------" << endl;
+	// cout<<"Initial State:"<<endl;
+ //  	initializedGame.printState();
+	// cout<<"----------"<<endl;
+	// cout<<"Final State:"<<endl;
+	// game.printState();
+	cout << "Exiting main" << endl;
 	return 0;
 }
 
