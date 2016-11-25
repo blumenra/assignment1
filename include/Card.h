6b52cbbf601211273758f947620ff50b646929df
blumenreich alon <blumenra@cs307row1-5.cs.bgu.ac.il>
2016-11-25 02:48:28 +0200

    Added new functions to Card.h

--- include/Card.h
+++ include/Card.h
@@ -20,16 +20,22 @@ enum Figure {
 
 class Card {
 private:
-  Shape shape;
+  	Shape shape;
 public:
-  virtual string toString() = 0; //Returns the string representation of the card "<value><shape>" exp: "12S" or "QD"
-  virtual ~Card();
+ 	Card(Shape shape);
+	virtual string toString() = 0; //Returns the string representation of the card "<value><shape>" exp: "12S" or "QD"
+ 	Shape getShape() const;
+ 	string shapeToString(Shape shape);
+ 	virtual ~Card();
 };
 
 class FigureCard : public Card {
 private:
 	Figure figure;
+	string figureToString(Figure figure) const;
 public:
+	FigureCard(Shape shape, Figure figure);
+	Figure getFigure() const;
 	virtual string toString() override;
 };
 
@@ -37,6 +43,8 @@ class NumericCard : public Card {
 private:
 	int number;
 public:
+	NumericCard(Shape shape, int number);
+	int getNumber() const;
 	virtual string toString() override;
 };
 
