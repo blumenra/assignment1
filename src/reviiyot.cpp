65645b4f083acf77a1982a47bb27e72a894b0de0
blumenreich alon <blumenra@cs314row2-1.cs.bgu.ac.il>
2016-11-29 17:02:14 +0200

    Created test functions for card and deck and stored all the code in there

--- src/reviiyot.cpp
+++ src/reviiyot.cpp
@@ -8,99 +8,34 @@
 
 using namespace std;
 
+void cardTest();
+void deckTest();
+void handTest();
+
 int main(int argc, char **argv) {
 
-Shape shape(Heart);
-
-cout << "creating new figure of King.." << endl;
-Figure figure(King);
-
-cout << "creating new figureCard.." << endl;
-Card* figuredCard = new FigureCard(shape, figure);
-
-cout << "creating new numericCard.." << endl;
-Card* numericCard = new NumericCard(shape, 1234);
-
-vector<Card*> cards;
-cout << "Is this cards empty?: " << cards.empty() << endl;
-
-cards.push_back(figuredCard);
-cards.push_back(numericCard);
-
-// cards.pop_back();
 
 
-Deck deck(cards);
+	// cardTest();
+	// deckTest();
 
-cout << "Is this deck empty?: " << deck.isEmpty() << endl;
 
-cout << "The deck's size?: " << deck.getNumberOfCards() << endl;
-
-cout << "Deck: " << deck.toString() << endl;
-
-// cout << "toString of fetched card: " << deck.fetchCard()->toString() + "\n" << endl;
-
-cout << "Is this deck empty?: " << deck.isEmpty() << endl;
-
-cout << "The deck's size?: " << deck.getNumberOfCards() << endl;
-
-cout << "Deck: " << deck.toString() << endl;
-
-vector<Card*> cards2;
-
-cards2 = deck.giveCards(-1);
-
-Deck deck2(cards2);
-
-cout << "Is this deck2 empty?: " << deck2.isEmpty() << endl;
-
-cout << "The deck2's size?: " << deck2.getNumberOfCards() << endl;
-
-cout << "Deck2: " << deck2.toString() << endl;
-
-// cout << "toString of fetched card: " << deck.fetchCard()->toString() + "\n" << endl;
-
-cout << "Is this deck2 empty?: " << deck2.isEmpty() << endl;
-
-cout << "The deck2's size?: " << deck2.getNumberOfCards() << endl;
-
-cout << "Deck2: " << deck2.toString() << endl;
-
-cout << "Is this deck empty?: " << deck.isEmpty() << endl;
-
-cout << "The deck's size?: " << deck.getNumberOfCards() << endl;
-
-cout << "Deck: " << deck.toString() << endl;
-
-//CARD TEST START----------------------------
-
-// cout << "start main:" << endl;
-
-// cout << "creating new shape of Heart.." << endl;
-// Shape shape(Heart);
-
-// cout << "creating new figure of Ace.." << endl;
-// Figure figure(Ace);
-
-// cout << "creating new figureCard.." << endl;
-// Card* figuredCard = new FigureCard(shape, figure);
-
-// cout << "toString of card: " << figuredCard->toString() + "\n" << endl;
-
-// cout << "getFigure: " << figuredCard->getStrValue() + "\n" << endl;
+	cout << "Exiting main" << endl;
 
 
 
-// cout << "creating new numericCard.." << endl;
-// Card* numericCard = new NumericCard(shape, 1234);
-
-// cout << "toString of card: " << numericCard->toString() + "\n" << endl;
-
-// cout << "getStrNumber of numericCard:" << numericCard->getStrValue() + "\n" << endl;
-
-//CARD TEST END--------------------------
 
 
+
+
+
+
+
+
+
+
+// ORIGINAL TEXT- DO NOT TOUCH!!!-------------------------------------------------
+	
 	// char* configurationFile = argv[1];
 
 	// Game game = Game(configurationFile);
@@ -117,7 +52,106 @@ cout << "Deck: " << deck.toString() << endl;
 	// cout<<"----------"<<endl;
 	// cout<<"Final State:"<<endl;
 	// game.printState();
-	cout << "Exiting main" << endl;
+
+// ORIGINAL TEXT- DO NOT TOUCH!!!-------------------------------------------------
+
 	return 0;
 }
 
+
+void cardTest() {
+
+	cout << "start main:" << endl;
+
+	cout << "creating new shape of Heart.." << endl;
+	Shape shape(Heart);
+
+	cout << "creating new figure of Ace.." << endl;
+	Figure figure(Ace);
+
+	cout << "creating new figureCard.." << endl;
+	Card* figuredCard = new FigureCard(shape, figure);
+
+	cout << "toString of card (figured): " << figuredCard->toString() + "\n" << endl;
+
+	cout << "getFigure: " << figuredCard->getStrValue() + "\n" << endl;
+
+
+	cout << "creating new numericCard.." << endl;
+	Card* numericCard = new NumericCard(shape, 1234);
+
+	cout << "toString of card (numeric): " << numericCard->toString() + "\n" << endl;
+
+	cout << "getStrNumber of numericCard:" << numericCard->getStrValue() + "\n" << endl;
+
+}
+
+void deckTest() {
+
+	Shape shape(Heart);
+
+	cout << "creating new figure of King.." << endl;
+	Figure figure(King);
+
+	cout << "creating new figureCard.." << endl;
+	Card* figuredCard = new FigureCard(shape, figure);
+
+	cout << "creating new numericCard.." << endl;
+	Card* numericCard = new NumericCard(shape, 1234);
+
+	vector<Card*> cards;
+	cout << "Is this cards empty?: " << cards.empty() << endl;
+
+	cards.push_back(figuredCard);
+	cards.push_back(numericCard);
+
+	// cards.pop_back();
+
+
+	Deck deck(cards);
+
+	cout << "Is this deck empty?: " << deck.isEmpty() << endl;
+
+	cout << "The deck's size?: " << deck.getNumberOfCards() << endl;
+
+	cout << "Deck: " << deck.toString() << endl;
+
+	// cout << "toString of fetched card: " << deck.fetchCard()->toString() + "\n" << endl;
+
+	cout << "Is this deck empty?: " << deck.isEmpty() << endl;
+
+	cout << "The deck's size?: " << deck.getNumberOfCards() << endl;
+
+	cout << "Deck: " << deck.toString() << endl;
+
+	vector<Card*> cards2;
+
+	cards2 = deck.giveCards(1);
+
+	Deck deck2(cards2);
+
+	cout << "Is this deck2 empty?: " << deck2.isEmpty() << endl;
+
+	cout << "The deck2's size?: " << deck2.getNumberOfCards() << endl;
+
+	cout << "Deck2: " << deck2.toString() << endl;
+
+	// cout << "toString of fetched card: " << deck.fetchCard()->toString() + "\n" << endl;
+
+	cout << "Is this deck2 empty?: " << deck2.isEmpty() << endl;
+
+	cout << "The deck2's size?: " << deck2.getNumberOfCards() << endl;
+
+	cout << "Deck2: " << deck2.toString() << endl;
+
+	cout << "Is this deck empty?: " << deck.isEmpty() << endl;
+
+	cout << "The deck's size?: " << deck.getNumberOfCards() << endl;
+
+	cout << "Deck: " << deck.toString() << endl;
+
+}
+
+void handTest() {
+
+}
